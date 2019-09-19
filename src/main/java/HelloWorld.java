296fb3c09332556cb5bcc12b3df4e26da941fe39
Dave Bohrk <dave@ionicsecurity.com>
2019-09-19 11:22:31 -0400

    Updated POM with apache dependancy

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,10 @@
+import org.apache.commons.lang3.StringUtils;
+
+import static org.apache.commons.lang3.StringUtils.capitalize;
+
+public class HelloWorld {
+    public static <args> void  main(string[], args) {
+        System.out.println("Hello World!!!");
+        System.out.println(capitalize("hello world"));
+    }
+}
