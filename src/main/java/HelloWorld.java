ef388b11ff9b522268be4789a973fad75d90901e
Dave Bohrk <dave@ionicsecurity.com>
2019-09-19 12:46:52 -0400

    updated HelloWorld

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,10 +1,9 @@
 import org.apache.commons.lang3.StringUtils;
 
-import static org.apache.commons.lang3.StringUtils.capitalize;
-
 public class HelloWorld {
-    public static <args> void  main(string[], args) {
-        System.out.println("Hello World!!!");
-        System.out.println(capitalize("hello world"));
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!!!! ");
+        System.out.println(StringUtils.capitalize("hello world"));
     }
 }
